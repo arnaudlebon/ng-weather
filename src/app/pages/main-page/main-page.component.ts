5ede78281cec7685771cf60f838db35d91d9b6fe
Arnaud LEBON <alebon.pro@gmail.com>
2024-07-22 17:06:52 +0200

    clean

--- src/app/pages/main-page/main-page.component.ts
+++ src/app/pages/main-page/main-page.component.ts
@@ -2,7 +2,7 @@ import { Component, inject, OnDestroy } from "@angular/core";
 import { LocationService } from "app/services/location/location.service";
 import { WeatherFacadeService } from "app/services/weather/weather-facade.service";
 import { toObservable } from "@angular/core/rxjs-interop";
-import { Subscription } from "rxjs";
+import { Observable, Subscription } from "rxjs";
 
 @Component({
   selector: "app-main-page",
@@ -11,11 +11,11 @@ import { Subscription } from "rxjs";
 export class MainPageComponent implements OnDestroy {
   private readonly locationService = inject(LocationService);
   private readonly weatherService = inject(WeatherFacadeService);
+  private locations$ = toObservable(this.locationService.locations) as Observable<string[]>;
   private subscription: Subscription;
 
   constructor() {
-    const locations$ = toObservable(this.locationService.locations);
-    this.subscription = locations$.subscribe((locations) => {
+    this.subscription = this.locations$.subscribe((locations) => {
       this.weatherService.updateCurrentConditions(locations);
     });
   }
