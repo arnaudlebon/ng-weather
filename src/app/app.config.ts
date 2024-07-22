740bbf4384dc323924a3083315f9d98d4e62df3e
Arnaud LEBON <alebon.pro@gmail.com>
2024-07-22 18:01:32 +0200

    clean

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -14,8 +14,8 @@ export const appConfig: AppConfig = {
     apiUrl: environment.apiUrl,
     appId: environment.appId,
     iconUrl: environment.iconUrl,
-    /************************************************************/
-    /** You can customize cacheTTL there and set a few seconds **/ 
+    /********************************************************************/
+    /** You can customize cacheTTL there and set a few seconds to test **/ 
     cacheTTL: 2 * 60 * 60 * 1000 // 2 hours in milliseconds
     // cacheTTL: 30 * 1000 // 30 seconds in milliseconds
   };
