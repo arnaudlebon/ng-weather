81238bf848f63d2a40a0caafe43bc176e9beff66
Arnaud LEBON <alebon.pro@gmail.com>
2024-07-23 15:16:00 +0200

    add: form control zipcode

--- src/app/components/zipcode-entry/zipcode-entry.component.ts
+++ src/app/components/zipcode-entry/zipcode-entry.component.ts
@@ -1,9 +1,10 @@
-import { Component, inject, signal } from '@angular/core';
+import { Component, DestroyRef, inject, signal } from '@angular/core';
 import { LocationService } from '../../services/location/location.service';
 import { WeatherFacadeService } from '../../services/weather/weather-facade.service';
 import { catchError, map, switchMap, take } from 'rxjs/operators';
 import { combineLatest, Observable, of, throwError } from 'rxjs';
 import { toObservable, takeUntilDestroyed  } from '@angular/core/rxjs-interop';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-zipcode-entry',
@@ -12,23 +13,27 @@ import { toObservable, takeUntilDestroyed  } from '@angular/core/rxjs-interop';
 export class ZipcodeEntryComponent {
   protected readonly locationService = inject(LocationService);
   protected readonly weatherService = inject(WeatherFacadeService);
-  
-  private locations$ = toObservable(this.locationService.locations) as Observable<string[]>;
+  protected readonly destroyRef = inject(DestroyRef);
 
+  private locations$ = toObservable(this.locationService.locations) as Observable<string[]>;
+  
+  zipcodeControl = new FormControl<string>('', [Validators.required]);
   errorMessage = signal<string | null>(null);
 
-  addLocation(zipcode: string) {
+  addLocation() {
+    const zipcode = this.zipcodeControl.value;
     this.checkDuplicateLocation(zipcode).pipe(
       switchMap(validZip => this.checkCanFetchWeather(validZip)),
       catchError(error => {
         this.errorMessage.set(error.message);
         return of(null);
       }),
-      takeUntilDestroyed()
+      takeUntilDestroyed(this.destroyRef)
     ).subscribe(weather => {
       if (weather) {
         this.locationService.addLocation(zipcode);
         this.errorMessage.set(null);
+        this.zipcodeControl.reset();
       }
     });
   }
