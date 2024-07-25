8d5e21c728a0f3dba0f0aac29c6c3433e91bba6b
Arnaud LEBON <alebon.pro@gmail.com>
2024-07-25 07:33:26 +0200

    fix: missing any

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,6 +1,6 @@
 import { ModuleWithProviders }  from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-import {MainPageComponent} from "./pages/main-page/main-page.component";
+import { MainPageComponent } from "./pages/main-page/main-page.component";
 import { ForecastsListComponent } from './pages/forecasts-list/forecasts-list.component';
 
 const appRoutes: Routes = [
@@ -11,4 +11,4 @@ const appRoutes: Routes = [
     path: 'forecast/:zipcode', component: ForecastsListComponent
   }
 ];
-export const routing: ModuleWithProviders<any> = RouterModule.forRoot(appRoutes, {});
+export const routing: ModuleWithProviders<RouterModule> = RouterModule.forRoot(appRoutes, {});
